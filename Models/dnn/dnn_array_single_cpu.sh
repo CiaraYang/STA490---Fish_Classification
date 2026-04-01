a5f5e987304acf4d65cb2dda454beaa2fe8c2c69
Yinuo Yang <ynyang@login1.int.fir.alliancecan.ca>
2026-03-31 19:36:58 -0700

    update the dnn server code to the focal loss version

--- Models/dnn/dnn_array_single_cpu.sh
+++ Models/dnn/dnn_array_single_cpu.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-#SBATCH --mem-per-cpu=2500M      # increase as needed
+#SBATCH --mem-per-cpu=2500M
 #SBATCH --time=0:180:00
 #SBATCH --cpus-per-task=1
-#SBATCH --array=0-215%500
-
-BATCH=$1
+#SBATCH --array=1-216%500
 
 module load r/4.4
 
-Rscript --vanilla dnn_hyperparameter_tuning_server.R -m $(($SLURM_ARRAY_TASK_ID + 1 + $BATCH*10000  ))
+Rscript --vanilla dnn_hyperparameter_tuning_server.R -m $SLURM_ARRAY_TASK_ID
